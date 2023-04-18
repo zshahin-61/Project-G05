e68b8e3dba3f84caedec75d3dbace18df501b845
GolnazCh <golnaz.chehrazi@gmail.com>
2023-04-18 00:42:15 -0400

    commited

--- app/src/main/java/com/example/project_g05/ParkDetailsFragment.kt
+++ app/src/main/java/com/example/project_g05/ParkDetailsFragment.kt
@@ -1,12 +1,68 @@
 package com.example.project_g05
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.project_g05.databinding.FragmentParkDetailsBinding
 
 
 class ParkDetailsFragment : Fragment(R.layout.fragment_park_details) {
+    private val TAG:String = "Park Details"
+
+    // binding variables
+    private var _binding: FragmentParkDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    // TODO: safe args class property
+    private val args: ParkDetailsFragmentArgs by navArgs()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // binding
+        _binding = FragmentParkDetailsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvTitle.text = args.argsFromPark?.fullName
+        binding.tvDescription.text = args.argsFromPark?.description
+
+        var strAddress=""
+        for(address in args.argsFromPark!!.addresses)
+            strAddress+="$address \n"
+        binding.tvAddress.text = strAddress
+
+        //binding.tvWebsite.url = "Brand: ${args.argsFromPark?.brand}"
+
+        val imageView: ImageView = binding.ivImage
+
+        Glide.with(requireContext())
+            .load(args.argsFromPark?.images)
+            .into(imageView);
+
+
+        Log.d(TAG, "Selected product from screen #1: ${args.argsFromPark}")
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 
 }
