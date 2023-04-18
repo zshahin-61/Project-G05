cff4196e6846888054a1dea15642b3e085d2b0fc
GolnazCh <golnaz.chehrazi@gmail.com>
2023-04-18 19:24:39 -0400

    commited

--- app/src/main/java/com/example/project_g05/ParkDetailsFragment.kt
+++ app/src/main/java/com/example/project_g05/ParkDetailsFragment.kt
@@ -1,13 +1,11 @@
 package com.example.project_g05
 
-import android.content.ClipData.Item
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.WebView
 import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
